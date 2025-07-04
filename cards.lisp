ee6294057da0e47ea26b5ff88eb8e3caaf974cc8
Avishek Gorai <avishekgorai@myyahoo.com>
2025-07-04 20:44:15 +0000

    Removed '#'.

--- cards.lisp
+++ cards.lisp
@@ -14,7 +14,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-(in-package #:cards)
+(in-package cards)
 
 (defvar my-hand
   (quote
